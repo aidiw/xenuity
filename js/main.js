55bee6991be61d526584d526a7a2b5cbdd4c139d
aidiw <68888676+aidiw@users.noreply.github.com>
2020-09-17 22:33:49 +0900

    js

--- js/main.js
+++ js/main.js
@@ -80,6 +80,58 @@
         // 클릭시 스크롤이동
 
 
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        ////////////about 영역 ////////////////
+        ///// 로드구역 /////
+
+        function isElementUnderBottom(elem, triggerDiff) {
+            const {
+                top
+            } = elem.getBoundingClientRect();
+            const {
+                innerHeight
+            } = window;
+            return top > innerHeight + (triggerDiff || 0);
+        }
+
+        function handleScroll() {
+            const elems = document.querySelectorAll('.up-on-scroll');
+            elems.forEach(elem => {
+                if (isElementUnderBottom(elem, -20)) {
+                    elem.style.opacity = "0";
+                    elem.style.transform = 'translateY(70px)';
+                } else {
+                    elem.style.opacity = "1";
+                    elem.style.transform = 'translateY(0px)';
+                }
+            })
+        } //////////handlescroll
+
+        window.addEventListener('scroll', handleScroll);
+
     }); ////// jQB //////////////////////////
     ////////////////////////////////////////
     ////////////////////////////////////////
