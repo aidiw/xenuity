4c5faa583194886807103bdca9b7469679a5be25
aidiw <68888676+aidiw@users.noreply.github.com>
2020-09-17 22:04:26 +0900

    new

---
+++ js/main.js
@@ -0,0 +1,85 @@
+    console.log("로딩완료");
+    /*마우스 커서*/
+    function resultFun(x) {
+        var positionLeft = x.clientX;
+        var positionTop = x.clientY;
+        document.getElementById('mouse').style.left = positionLeft - 10 + "px";
+        document.getElementById('mouse').style.top = positionTop - 10 + "px";
+    }
+    if (document.addEventListener) {
+        document.addEventListener("mousemove", resultFun);
+    } else if (document.attachEvent) {
+        document.attachEvent("onmousemove", resultFun);
+        //attachEvent는 IE8이하와 오페라에서 사용하는 명령어이다 };
+    }
+
+    ///// 전역변수 ///////////////////////////////
+
+    // 1. 페이지번호
+    var pno = 0;
+    // 2. 전체 페이지수
+    const totnum = 7;
+    // const는 변수 var와 달리 변경불가한 상수를 말한다!
+    // 3. 광스크롤방지
+    var psts = 0; //(0-허용,1-불허용)
+
+    /////////////////////////////////////////////
+
+
+    $(function () { /// jQB //////////////////
+        $(document).on("mousewheel DOMMouseScroll",
+            function (e) {
+
+                /////// 광스크롤막기 /////////////////
+                if (psts === 1) return true; //돌아가!
+                psts = 1; //잠금(기존0값을 변경)
+                setTimeout(function () {
+                    psts = 0;
+                }, 600); /// 타임아웃 ///
+                /////////광스크롤막기/////////////////
+
+                e = window.event || e;
+
+                var delta = e.detail ? e.detail : e.wheelDelta;
+                if (/Firefox/i.test(navigator.userAgent)) {
+                    //console.log("난파폭!");
+                    delta = -delta; //부호를 반대로!
+                } ////// if문 ////////////////////////
+
+                // 음수일때 아랫방향
+                if (delta < 0) {
+                    pno++; //1씩증가
+                    // 한계페이지번호 마지막번호에 고정!
+                    if (pno === totnum) pno = totnum - 1;
+                } /// if ///////////
+                // 양수일때 윗방향
+                else {
+                    pno--; //1씩감소
+                    // 한계페이지번호 첫번호에 고정!
+                    if (pno === -1) pno = 0;
+                } /// else ////////
+
+                //console.log("페이지번호:" + pno);
+
+                // 4. 해당순번 페이지 left 위치값 구하기
+                var pgpos = $(".page").eq(pno).offset().left;
+
+                console.log("이동페이지위치:" + pgpos);
+
+                // 5. 페이지 이동 애니메이션
+                $("html,body").stop().animate({
+                    scrollLeft: pgpos + "px"
+                }, 600, "easeInOutQuint");
+
+                // 스크롤시 숫자 카운트 증가
+                $(".count").text(pno + 1);
+
+            }); //////////////// mousewheel ////////////////
+
+
+        // 클릭시 스크롤이동
+
+
+    }); ////// jQB //////////////////////////
+    ////////////////////////////////////////
+    ////////////////////////////////////////
