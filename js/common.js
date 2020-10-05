e7f6c97ddbadded99bf9e8ea8246675733f783b4
aidiw <68888676+aidiw@users.noreply.github.com>
2020-10-05 13:14:15 +0900

    up

--- js/common.js
+++ js/common.js
@@ -2,54 +2,94 @@
     $("body").css("display", "none");
     $("body").fadeIn(2000);
     $("a.transition").click(function (event) {
-        event.preventDefault();
-        linkLocation = this.href;
-        $("body").fadeOut(1000, redirectPage);
+    	event.preventDefault();
+    	linkLocation = this.href;
+    	$("body").fadeOut(1000, redirectPage);
     });
 
     function redirectPage() {
-        window.location = linkLocation;
+    	window.location = linkLocation;
     }
 
 
 
 
     $(function () { ///////JQB///////
-        console.log("로딩완료");
-        /*마우스 커서*/
+    	console.log("로딩완료");
+    	/*마우스 커서*/
 
 
-        ////////////about 영역 ////////////////
-        ///// 로드구역 /////
+    	////////////about 영역 ////////////////
+    	///// 로드구역 /////
 
-        function isElementUnderBottom(elem, triggerDiff) {
-            const {
-                top
-            } = elem.getBoundingClientRect();
-            const {
-                innerHeight
-            } = window;
-            return top > innerHeight + (triggerDiff || 0);
-        }
+    	function isElementUnderBottom(elem, triggerDiff) {
+    		const {
+    			top
+    		} = elem.getBoundingClientRect();
+    		const {
+    			innerHeight
+    		} = window;
+    		return top > innerHeight + (triggerDiff || 0);
+    	}
 
-        function handleScroll() {
-            const elems = document.querySelectorAll('.up-on-scroll');
-            elems.forEach(elem => {
-                if (isElementUnderBottom(elem, -20)) {
-                    elem.style.opacity = "0";
-                    elem.style.transform = 'translateY(70px)';
-                } else {
-                    elem.style.opacity = "1";
-                    elem.style.transform = 'translateY(0px)';
-                }
-            })
-        } //////////handlescroll
+    	function handleScroll() {
+    		const elems = document.querySelectorAll('.up-on-scroll');
+    		elems.forEach(elem => {
+    			if (isElementUnderBottom(elem, -20)) {
+    				elem.style.opacity = "0";
+    				elem.style.transform = 'translateY(70px)';
+    			} else {
+    				elem.style.opacity = "1";
+    				elem.style.transform = 'translateY(0px)';
+    			}
+    		})
+    	} //////////handlescroll
 
-        window.addEventListener('scroll', handleScroll);
+    	window.addEventListener('scroll', handleScroll);
 
 
 
-        //////////////////////// 호버 버튼 부분 //////////////////
-        
+    	//////////////////////// 호버 버튼 부분 //////////////////
+    	$('.moreb, .downb').mouseleave(function (e) {
+    		TweenMax.to(this, 0.3, {
+    			scale: 1
+    		});
+    		TweenMax.to('.morew, .downw', 0.3, {
+    			scale: 1,
+    			x: 0,
+    			y: 0
+    		});
+    	});
 
+    	$('.moreb, .downb').mouseenter(function (e) {
+    		TweenMax.to(this, 0.3, {
+    			transformOrigin: '0 0',
+    			scale: 1
+    		});
+    		TweenMax.to('.morew, .downw', 0.3, {
+    			scale: 1.3
+    		});
+    	});
+
+    	$('.moreb, .downb').mousemove(function (e) {
+    		callParallax(e);
+    	});
+
+    	function callParallax(e) {
+    		parallaxIt(e, '.morew, .downw', 60);
+    	}
+
+    	function parallaxIt(e, target, movement) {
+    		var $this = $('.morew, .downw');
+    		var boundingRect = $this[0].getBoundingClientRect();
+    		var relX = e.pageX - boundingRect.left;
+    		var relY = e.pageY - boundingRect.top;
+    		var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    		TweenMax.to(target, 0.3, {
+    			x: (relX - boundingRect.width / 2) / boundingRect.width * movement,
+    			y: (relY - boundingRect.height / 2 - scrollTop) / boundingRect.width * movement,
+    			ease: Power2.easeOut
+    		});
+    	}
     }) ////////JQB///////
--- js/main.js
+++ js/main.js
@@ -9,104 +9,78 @@
 
 
     $(function () { /// jQB //////////////////
-        // 스크롤 스와이퍼 플러그인//
-        var swiper = new Swiper({
-            el: '.swiper-container',
-            initialSlide: 0,
-            spaceBetween: 0,
-            slidesPerView: 1,
-            centeredSlides: true,
-            grabCursor: true,
-            mousewheel: {
-                enabled: true,
-            },
-            keyboard: {
-                enabled: true,
-            },
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            fadeEffect: {
-                crossFade: true
-            },
-            speed: 1500,
-        });
+    	// 스크롤 스와이퍼 플러그인//
+    	var swiper = new Swiper({
+    		el: '.swiper-container',
+    		initialSlide: 0,
+    		spaceBetween: 0,
+    		slidesPerView: 1,
+    		centeredSlides: true,
+    		grabCursor: true,
+    		mousewheel: {
+    			enabled: true,
+    		},
+    		keyboard: {
+    			enabled: true,
+    		},
+    		pagination: {
+    			el: '.swiper-pagination',
+    		},
+    		navigation: {
+    			nextEl: '.swiper-button-next',
+    			prevEl: '.swiper-button-prev',
+    		},
+    		fadeEffect: {
+    			crossFade: true
+    		},
+    		speed: 1500,
+    	});
 
-        ////////////////////////스와이프 커버플로우 ///////////////////////////////////
-    /* 부드러운 화면전환 */
-    $("body").css("display", "none");
-    $("body").fadeIn(2000);
-    $("a.transition").click(function (event) {
-        event.preventDefault();
-        linkLocation = this.href;
-        $("body").fadeOut(1000, redirectPage);
-    });
+    	////////////////////////스와이프 커버플로우 ///////////////////////////////////
+    	/* 부드러운 화면전환 */
+    	$("body").css("display", "none");
+    	$("body").fadeIn(2000);
+    	$("a.transition").click(function (event) {
+    		event.preventDefault();
+    		linkLocation = this.href;
+    		$("body").fadeOut(1000, redirectPage);
+    	});
 
-    function redirectPage() {
-        window.location = linkLocation;
-    }
-
-
-
-
-
-        ////////////about 영역 ////////////////
-        ///// 로드구역 /////
-
-        function isElementUnderBottom(elem, triggerDiff) {
-            const {
-                top
-            } = elem.getBoundingClientRect();
-            const {
-                innerHeight
-            } = window;
-            return top > innerHeight + (triggerDiff || 0);
-        }
-
-        function handleScroll() {
-            const elems = document.querySelectorAll('.up-on-scroll');
-            elems.forEach(elem => {
-                if (isElementUnderBottom(elem, -20)) {
-                    elem.style.opacity = "0";
-                    elem.style.transform = 'translateY(70px)';
-                } else {
-                    elem.style.opacity = "1";
-                    elem.style.transform = 'translateY(0px)';
-                }
-            })
-        } //////////handlescroll
-
-        window.addEventListener('scroll', handleScroll);
+    	function redirectPage() {
+    		window.location = linkLocation;
+    	}
 
 
 
 
 
+    	////////////about 영역 ////////////////
+    	///// 로드구역 /////
 
+    	function isElementUnderBottom(elem, triggerDiff) {
+    		const {
+    			top
+    		} = elem.getBoundingClientRect();
+    		const {
+    			innerHeight
+    		} = window;
+    		return top > innerHeight + (triggerDiff || 0);
+    	}
 
-        // 스크롤 스와이퍼 플러그인//
+    	function handleScroll() {
+    		const elems = document.querySelectorAll('.up-on-scroll');
+    		elems.forEach(elem => {
+    			if (isElementUnderBottom(elem, -20)) {
+    				elem.style.opacity = "0";
+    				elem.style.transform = 'translateY(70px)';
+    			} else {
+    				elem.style.opacity = "1";
+    				elem.style.transform = 'translateY(0px)';
+    			}
+    		})
+    	} //////////handlescroll
 
-
-
-        $(".page").hover(
-            function () { // over
-                $(this).css({
-                    transform: "rotateY(40deg) scale(.8)",
-                    transformStyle: "preserve-3d",
-                    transition: "transform .4s",
-
-                });
-            },
-            function () { // out
-                //            $(this).css({
-                //                transform:"rotateY(0deg)"
-                //                
-                //            });
-            }); //////// hover //////////////
+    	window.addEventListener('scroll', handleScroll);
 
 
 
@@ -114,6 +88,118 @@
 
 
 
+    	// 스크롤 스와이퍼 플러그인//
+
+
+    	/////////////////////마우스 이동시 섹션 무브///////////////////////
+
+    	/* Store the element in el */
+//    	let ele = document.getElementsByClassName('tilt');
+    	let ele = $('.tilt');
+
+    	/* Get the height and width of the element */
+    	const height = ele.height();
+    	const width = ele.width();
+
+    	/*
+    	 * Add a listener for mousemove event
+    	 * Which will trigger function 'handleMove'
+    	 * On mousemove
+    	 */
+		
+		
+		ele.mousemove(function(e){
+			
+    		/*
+    		 * Get position of mouse cursor
+    		 * With respect to the element
+    		 * On mouseover
+    		 */
+    		/* Store the x position */
+    		const xVal = e.pageX;
+    		/* Store the y position */
+    		const yVal = e.pageY;
+
+    		/*
+    		 * Calculate rotation valuee along the Y-axis
+    		 * Here the multiplier 20 is to
+    		 * Control the rotation
+    		 * You can change the value and see the results
+    		 */
+    		const yRotation = 3 * ((xVal - width / 2) / width);
+
+    		/* Calculate the rotation along the X-axis */
+    		const xRotation = -3 * ((yVal - height / 2) / height);
+
+    		/* Generate string for CSS transform property */
+    		const string = 'perspective(500px) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)';
+
+    		/* Apply the calculated transformation */
+//    		obj.style.transform = string
+			$(this).css({
+				transform: string
+			});
+		});
+//			ele[i].addEventListener('mousemove', function(){handleMove(event,ele[i]);});
+		
+
+    	/* Define function a */
+    	function handleMove(e,obj) {
+			console.log(e+"/"+obj);
+    		/*
+    		 * Get position of mouse cursor
+    		 * With respect to the element
+    		 * On mouseover
+    		 */
+    		/* Store the x position */
+    		const xVal = e.layerX
+    		/* Store the y position */
+    		const yVal = e.layerY
+
+    		/*
+    		 * Calculate rotation valuee along the Y-axis
+    		 * Here the multiplier 20 is to
+    		 * Control the rotation
+    		 * You can change the value and see the results
+    		 */
+    		const yRotation = 2 * ((xVal - width / 1) / width)
+
+    		/* Calculate the rotation along the X-axis */
+    		const xRotation = -2 * ((yVal - height / 1) / height)
+
+    		/* Generate string for CSS transform property */
+    		const string = 'perspective(500px) scale(1.08) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+
+    		/* Apply the calculated transformation */
+//    		obj.style.transform = string
+			obj.css({
+				transform: string
+			});
+    	}///////////////////////////////마우스무브화면무브//////////////////////////////////
+
+		
+		
+		//////////////////////////////마그네틱 버튼////////////////////////////
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
     }); ////// jQB //////////////////////////
     ////////////////////////////////////////
