983b6adc4cc2f44bb5d6b3012f7503cec991f678
aidiw <68888676+aidiw@users.noreply.github.com>
2020-10-06 11:45:28 +0900

    mouse up

--- js/main.js
+++ js/main.js
@@ -5,6 +5,21 @@
     console.log("페이지구분:" + pgname);
 
 
+    //    /*마우스 커서*/
+    //
+    //    function resultFun(x) {
+    //    	var positionLeft = x.clientX;
+    //    	var positionTop = x.clientY;
+    //    	document.getElementById('mouse').style.left = positionLeft - 10 + "px";
+    //    	document.getElementById('mouse').style.top = positionTop - 10 + "px";
+    //    }
+    //    if (document.addEventListener) {
+    //    	document.addEventListener("mousemove", resultFun);
+    //    } else if (document.attachEvent) {
+    //    	document.attachEvent("onmousemove", resultFun);
+    //    	//attachEvent는 IE8이하와 오페라에서 사용하는 명령어이다 };
+    //    }
+    //
 
 
 
@@ -51,12 +66,62 @@
     	}
 
 
+    	//////////////////////// 호버 버튼 부분 //////////////////
+    	$('.moreb, .downb').mouseleave(function (e) {
+    		TweenMax.to(this, 0.3, {
+    			scale: 1
+    		});
+    		TweenMax.to('.morew, .downw', 0.3, {
+    			scale: 1,
+    			x: 0,
+    			y: 0
+    		});
+    	});
+
+    	$('.moreb, .downb').mouseenter(function (e) {
+    		TweenMax.to(this, 0.3, {
+    			transformOrigin: '0 0',
+    			scale: 1
+    		});
+    		TweenMax.to('.morew, .downw', 0.3, {
+    			scale: 1.3
+    		});
+    	});
+
+    	$('.moreb, .downb').mousemove(function (e) {
+    		callParallax(e);
+    	});
+
+    	function callParallax(e) {
+    		parallaxIt(e, '.morew, .downw', 60);
+    	}
+
+    	function parallaxIt(e, target, movement) {
+    		var $this = $('.morew, .downw');
+    		var boundingRect = $this[0].getBoundingClientRect();
+    		var relX = e.pageX - boundingRect.left;
+    		var relY = e.pageY - boundingRect.top;
+    		var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    		TweenMax.to(target, 0.3, {
+    			x: (relX - boundingRect.width / 2) / boundingRect.width * movement,
+    			y: (relY - boundingRect.height / 2 - scrollTop) / boundingRect.width * movement,
+    			ease: Power2.easeOut
+    		});
+    	}////////////////////////////////////////////////////////////////////
+		/////////////////////////////호버버튼!///////////////////////////////
+
+
+
+
+
+
 
 
 
     	////////////about 영역 ////////////////
     	///// 로드구역 /////
-
+    	//////////////////////스크롤시 페이드효과//////////////////
     	function isElementUnderBottom(elem, triggerDiff) {
     		const {
     			top
@@ -90,104 +155,106 @@
 
     	/////////////////////마우스 이동시 섹션 무브///////////////////////
 
-    	    	/* Store the element in el */
-    	    	//    	let ele = document.getElementsByClassName('tilt');
-    	    	let ele = $('.tilt');
-    	
-    	    	/* Get the height and width of the element */
-    	    	const height = ele.height();
-    	    	const width = ele.width();
-    	
-    	    	/*
-    	    	 * Add a listener for mousemove event
-    	    	 * Which will trigger function 'handleMove'
-    	    	 * On mousemove
-    	    	 */
-    	
-    	
-    	    	ele.mousemove(function (e) {
-    	
-    	    		/*
-    	    		 * Get position of mouse cursor
-    	    		 * With respect to the element
-    	    		 * On mouseover
-    	    		 */
-    	    		/* Store the x position */
-    	    		const xVal = e.pageX;
-    	    		/* Store the y position */
-    	    		const yVal = e.pageY;
-    	
-    	    		/*
-    	    		 * Calculate rotation valuee along the Y-axis
-    	    		 * Here the multiplier 20 is to
-    	    		 * Control the rotation
-    	    		 * You can change the value and see the results
-    	    		 */
-    	    		const yRotation = 2 * ((xVal - width / 2) / width);
-    	
-    	    		/* Calculate the rotation along the X-axis */
-    	    		const xRotation = -2 * ((yVal - height / 2) / height);
-    	
-    	    		/* Generate string for CSS transform property */
-    	    		const string = 'perspective(500px) scale(1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)';
-    	
-    	
-    	
-    	    		/* Apply the calculated transformation */
-    	    		//    		obj.style.transform = string
-    	    		$(this).css({
-    	    			transform: string
-    	    		});
-    	
-    	
-    	    	});
+    	//    	/* Store the element in el */
+    	//    	//    	let ele = document.getElementsByClassName('tilt');
+    	//    	let ele = $('.tilt');
+    	//
+    	//    	/* Get the height and width of the element */
+    	//    	const height = ele.height();
+    	//    	const width = ele.width();
+    	//
+    	//    	/*
+    	//    	 * Add a listener for mousemove event
+    	//    	 * Which will trigger function 'handleMove'
+    	//    	 * On mousemove
+    	//    	 */
+    	//
+    	//
+    	//    	ele.mousemove(function (e) {
+    	//
+    	//    		/*
+    	//    		 * Get position of mouse cursor
+    	//    		 * With respect to the element
+    	//    		 * On mouseover
+    	//    		 */
+    	//    		/* Store the x position */
+    	//    		const xVal = e.pageX;
+    	//    		/* Store the y position */
+    	//    		const yVal = e.pageY;
+    	//
+    	//    		/*
+    	//    		 * Calculate rotation valuee along the Y-axis
+    	//    		 * Here the multiplier 20 is to
+    	//    		 * Control the rotation
+    	//    		 * You can change the value and see the results
+    	//    		 */
+    	//    		const yRotation = 2 * ((xVal - width / 2) / width);
+    	//
+    	//    		/* Calculate the rotation along the X-axis */
+    	//    		const xRotation = -2 * ((yVal - height / 2) / height);
+    	//
+    	//    		/* Generate string for CSS transform property */
+    	//    		const string = 'perspective(500px) scale(1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)';
+    	//
+    	//
+    	//
+    	//    		/* Apply the calculated transformation */
+    	//    		//    		obj.style.transform = string
+    	//    		$(this).css({
+    	//    			transform: string
+    	//    		});
+    	//
+    	//
+    	//    	});
+    	//
+    	//    	ele[i].addEventListener('mousemove', function () {
+    	//    		handleMove(event, ele[i]);
+    	//    	});
+    	//
 
-    				ele[i].addEventListener('mousemove', function(){handleMove(event,ele[i]);});
 
-
-
-    	    	/* Store the element in el */
-    	    	//    	let ele = document.getElementsByClassName('tilt');
-//    	    	let ele = $('.tilt');
-//    	
-//    	    	/* Get the height and width of the element */
-//    	    	const height = ele.height();
-//    	    	const width = ele.width();
-//
-//    	    	/* Define function a */
-//    	    	function handleMove(e,obj) {
-//    				console.log(e+"/"+obj);
-//    	    		/*
-//    	    		 * Get position of mouse cursor
-//    	    		 * With respect to the element
-//    	    		 * On mouseover
-//    	    		 */
-//    	    		/* Store the x position */
-//    	    		const xVal = e.layerX
-//    	    		/* Store the y position */
-//    	    		const yVal = e.layerY
-//    	
-//    	    		/*
-//    	    		 * Calculate rotation valuee along the Y-axis
-//    	    		 * Here the multiplier 20 is to
-//    	    		 * Control the rotation
-//    	    		 * You can change the value and see the results
-//    	    		 */
-//    	    		const yRotation = 2 * ((xVal - width / 1) / width)
-//    	
-//    	    		/* Calculate the rotation along the X-axis */
-//    	    		const xRotation = -2 * ((yVal - height / 1) / height)
-//    	
-//    	    		/* Generate string for CSS transform property */
-//    	    		const string = 'perspective(500px) scale(1.08) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
-//    	
-//    	    		/* Apply the calculated transformation */
-//    	//    		obj.style.transform = string
-//    				obj.css({
-//    					transform: string
-//    				});
-//    	    	}///////////////////////////////마우스무브화면무브//////////////////////////////////
-//
+    	/* Store the element in el */
+    	//    	let ele = document.getElementsByClassName('tilt');
+    	//    	    	let ele = $('.tilt');
+    	//    	
+    	//    	    	/* Get the height and width of the element */
+    	//    	    	const height = ele.height();
+    	//    	    	const width = ele.width();
+    	//
+    	//    	    	/* Define function a */
+    	//    	    	function handleMove(e,obj) {
+    	//    				console.log(e+"/"+obj);
+    	//    	    		/*
+    	//    	    		 * Get position of mouse cursor
+    	//    	    		 * With respect to the element
+    	//    	    		 * On mouseover
+    	//    	    		 */
+    	//    	    		/* Store the x position */
+    	//    	    		const xVal = e.layerX
+    	//    	    		/* Store the y position */
+    	//    	    		const yVal = e.layerY
+    	//    	
+    	//    	    		/*
+    	//    	    		 * Calculate rotation valuee along the Y-axis
+    	//    	    		 * Here the multiplier 20 is to
+    	//    	    		 * Control the rotation
+    	//    	    		 * You can change the value and see the results
+    	//    	    		 */
+    	//    	    		const yRotation = 2 * ((xVal - width / 1) / width)
+    	//    	
+    	//    	    		/* Calculate the rotation along the X-axis */
+    	//    	    		const xRotation = -2 * ((yVal - height / 1) / height)
+    	//    	
+    	//    	    		/* Generate string for CSS transform property */
+    	//    	    		const string = 'perspective(500px) scale(1.08) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+    	//    	
+    	//    	    		/* Apply the calculated transformation */
+    	//    	//    		obj.style.transform = string
+    	//    				obj.css({
+    	//    					transform: string
+    	//    				});
+    	//    	    	}///////////////////////////////마우스무브화면무브//////////////////////////////////
+    	//
 
 
     	//////////////////////////////마그네틱 버튼////////////////////////////
